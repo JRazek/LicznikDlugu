c1b1ba9a8fdd8c086907dba1c0e55e307b0be6e4
Jakub <jakub.jakubecky123@gmail.com>
2020-11-04 23:59:07 +0100

    segment tree done, updates to go

--- main.cpp
+++ main.cpp
@@ -4,107 +4,115 @@
 #include<string>
 #include <vector>
 using namespace std;
+using stringContainer = deque<int>;
 struct Range{
-    int min, max;
+    int min;
+    int max;
+    char value;
+    Range(int min, int max, char v){
+        this->min = min;
+        this->max = max;
+        this->value = v;
+    }
     Range(int min, int max){
         this->min = min;
         this->max = max;
     }
+    static Range * commonPart(Range * r1, Range * r2){
+        int min = r1->min > r2->min ? r1->min : r2->min;
+        int max = r1->max < r2->max ? r1->max : r2->max;
+        return new Range(min, max);
+    }
 };
-string merge2Num(string &num1, int multiplier1, string &num2, int multiplier2){
-    //multiplier is the power of then that the number is multiplied number2 should be equal or smaller than 1
-    int additionalZeros = multiplier1 - multiplier2;
-    for(int i = 0; i < additionalZeros; i ++){
-        num1 += "0";
-    }
-    stack<int> queue;
-    queue.push(0);
-    for (int i = 0; i < num2.size(); i ++) {
-        int indexInNum1 = num1.size() - i - 1;
-        int indexInNum2 = num2.size() - i - 1;
-        int newNum = (num1[indexInNum1] -'0') + (num2[indexInNum2] -'0') + queue.top();
-        queue.pop();
-        queue.push(newNum / 10);
-        newNum %= 10;
-        num1[indexInNum1] = to_string(newNum)[0];
-    }
 
-    return num1;
+string addNumbers(const string &num1, const string &num2){
+    string result = "0" + num1;
+
+    int nextLineNum = 0;
+    int i = 0;
+    while (i < num2.size()){
+        int ind1 = result.size() - i - 1;
+        int ind2 = num2.size() - i - 1;
+
+        int newNum = (result[ind1] - '0') + (num2[ind2] - '0') + nextLineNum;
+        nextLineNum = newNum / 10;
+        newNum %= 10;
+        result[ind1] = to_string(newNum)[0];
+        i++;
+    }
+    result[0] = to_string(nextLineNum)[0];
+    return result;
 }
 struct SegmentTree{
     struct BinaryNode{
-        BinaryNode * parent;
-
-        BinaryNode * left;
-        BinaryNode * right;
-
-        int powerOf10;
-        string value = "";
-
-        BinaryNode(string v, int p){
-            this->value = v;
-            this->powerOf10 = p;
-        }
-        BinaryNode(BinaryNode * left, BinaryNode * right, string v, int p){
-            this->left = left;
-            this->right = right;
-            this->value = v;
-            this->powerOf10 = p;
+        int id;
+        Range * range;
+        BinaryNode(int id, Range * r){
+            this->id = id;
+            this->range = r;
         }
     };
 
     BinaryNode * root;
 
-    string internal;
-    string external;
-
     int height;
-    vector< vector<BinaryNode *> > floors;
-
-    SegmentTree(string in, string out){
-        this->internal = in;
-        this->external = out;
-        float logResult = log2(in.size() + out.size());
-        bool integer = ((int)logResult) == logResult ? true : false;
-        this->height = (int)logResult + !integer + 1;
-        preprocessSegmentTree();
+    int firstFloorSize;
+    vector< BinaryNode * > nodes;
+    SegmentTree(string sum){
+        float logVal = log2(sum.size());
+        this->firstFloorSize = pow(2, (int)logVal + (bool)((int)logVal != logVal));
+        this->height = log2(firstFloorSize) + 1;
+        buildTree();
     }
-    void preprocessSegmentTree(){
-        for(int i = 0 ; i < height; i ++){
-            floors.push_back(vector<BinaryNode *>());
-            int currFloorSize = pow(2, height - i - 1);
-            for(int j = 0; j < currFloorSize; j ++){
-                if(i == 0){
-                    bool odd = (j % 2);
-                    string value;
-                    value = odd ? internal[j / 2] : external[j / 2];
-
-                    BinaryNode * binaryNode = new BinaryNode(value, currFloorSize - j/2 -1);
-
-                    floors.at(i).push_back(binaryNode);
-                } else{
-                    BinaryNode * leftChild = floors.at(i - 1).at(j * 2);
-                    BinaryNode * rightChild = floors.at(i - 1).at(j * 2 + 1);
-
-                    string mergedValue = merge2Num(leftChild->value, leftChild->powerOf10, rightChild->value, rightChild->powerOf10);
-                    BinaryNode * binaryNode = new BinaryNode(leftChild, rightChild, mergedValue, leftChild->powerOf10);
-
-
-                    leftChild->parent = binaryNode;
-                    rightChild->parent = binaryNode;
-
-                    floors.at(i).push_back(binaryNode);
-                }
-                if(i == height - 1){
-                    this->root = floors.at(i)[0];
-                }
-            }
-            cout<<"";
+    void buildTree(){
+        int nodesCount = pow(2, height) - 1;
+        for(int i = 0; i < nodesCount; i ++){
+            nodes.push_back(new BinaryNode(i, new Range(0,0)));
         }
+        for(int i = 0 ; i < firstFloorSize; i ++){
+            BinaryNode * n = nodes[i +  pow(2, height - 1) - 1];
+            n->range->min = i;
+            n->range->max = i;
+        }
+        for(int i = nodesCount - 1 - firstFloorSize; i >= 0; i --){
+            BinaryNode * n = nodes[i];
+            n->range->min = getChild(i, true)->range->min;
+            n->range->max = getChild(i, false)->range->max;
+            getParent(i);
+        }
+    }
+    BinaryNode * getChild(int binaryNodeID, bool left){
+        return nodes[(binaryNodeID) * 2 + !left + 1];
+    }
+    BinaryNode * getParent(int binaryNodeID){
+        return nodes[(binaryNodeID - 1) / 2];
+    }
+    vector<BinaryNode *> rangeQuery(Range * range, int nodeID = 0){
+        BinaryNode * parent = nodes[nodeID];
+        BinaryNode * leftChild = getChild(nodeID, true);
+        BinaryNode * rightChild = getChild(nodeID, false);
+
+        if(range->min == parent->range->min && range->max == parent->range->max){
+            return {parent};
+        }
+
+
+        Range * commonLeft = Range::commonPart(range, leftChild->range);
+        Range * commonRight = Range::commonPart(range, rightChild->range);
+        vector<BinaryNode *> result;
+        if(commonLeft->min <= commonLeft->max){
+            vector<BinaryNode *> resLeft = rangeQuery(commonLeft, leftChild->id);
+            result.insert(result.begin(), resLeft.begin(), resLeft.end());
+        }
+        if(commonRight->min <= commonRight->max){
+            vector<BinaryNode *> resRight = rangeQuery(commonRight, rightChild->id);
+            result.insert(result.begin(), resRight.begin(), resRight.end());
+        }
+        return result;
     }
 };
 
-vector<string> split(string str, char divider){
+vector<string> split(string str, char divider = ' '){
     vector<string> result;
 
     string currWord = "";
@@ -129,10 +137,24 @@ int main() {
     string internal = line;
     getline(cin, line);
     string external = line;
-   // SegmentTree segmentTree(internal, external);
 
-   string s1 = "9";
-   string s2 = "2";
+    string sum = addNumbers(internal, external);
 
+    vector<Range *> intervals;
+
+    bool first = true;
+    for(int i = 0; i < sum.size(); i ++){
+        if(sum[i] == '9'){
+            if(first){
+                intervals.push_back(new Range(i, 0, '9'));
+                first = false;
+            }
+            intervals.back()->max = i;
+        }else{
+            first = true;
+        }
+    }
+    SegmentTree segmentTree(sum);
+    vector<SegmentTree::BinaryNode *> nodes = segmentTree.rangeQuery(new Range(0,4));
     return 0;
 }
